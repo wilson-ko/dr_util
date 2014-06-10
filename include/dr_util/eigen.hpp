b21e97c185e49d4958095d90e718436d1a4cc1ab
Maarten de Vries <maarten@de-vri.es>
2014-06-10 17:51:40 +0200

    (split-dr-util)Keep Eigen transforms as specific as possible.

--- include/dr_util/eigen.hpp
+++ include/dr_util/eigen.hpp
@@ -10,43 +10,43 @@ namespace dr {
 		inline Eigen::Vector3d z() { return Eigen::Vector3d{0, 0, 1}; };
 	}
 
-	inline Eigen::Affine3d translate(Eigen::Vector3d translation) {
-		return Eigen::Affine3d{Eigen::Translation3d{translation}};
+	inline Eigen::Translation3d translate(Eigen::Vector3d translation) {
+		return Eigen::Translation3d{translation};
 	}
 
-	inline Eigen::Affine3d translate(double x, double y, double z) {
+	inline Eigen::Translation3d translate(double x, double y, double z) {
 		return translate(Eigen::Vector3d{x, y, z});
 	}
 
-	inline Eigen::Affine3d rotate(double angle, Eigen::Vector3d const & axis) {
-		return Eigen::Affine3d{Eigen::AngleAxisd{angle, axis}};
+	inline Eigen::AngleAxisd rotate(double angle, Eigen::Vector3d const & axis) {
+		return Eigen::AngleAxisd{angle, axis};
 	}
 
-	inline Eigen::Affine3d rotate(double angle, Eigen::Vector3d const & axis, Eigen::Vector3d const & pivot_point) {
+	inline Eigen::Isometry3d rotate(double angle, Eigen::Vector3d const & axis, Eigen::Vector3d const & pivot_point) {
 		return dr::translate(pivot_point) * Eigen::AngleAxisd{angle, axis} * dr::translate(-pivot_point);
 	}
 
-	inline Eigen::Affine3d rotate_x(double angle) {
+	inline Eigen::AngleAxisd rotate_x(double angle) {
 		return dr::rotate(angle, axes::x());
 	}
 
-	inline Eigen::Affine3d rotate_x(double angle, Eigen::Vector3d const & pivot_point) {
+	inline Eigen::Isometry3d rotate_x(double angle, Eigen::Vector3d const & pivot_point) {
 		return dr::rotate(angle, axes::x(), pivot_point);
 	}
 
-	inline Eigen::Affine3d rotate_y(double angle) {
+	inline Eigen::AngleAxisd rotate_y(double angle) {
 		return dr::rotate(angle, axes::y());
 	}
 
-	inline Eigen::Affine3d rotate_y(double angle, Eigen::Vector3d const & pivot_point) {
+	inline Eigen::Isometry3d rotate_y(double angle, Eigen::Vector3d const & pivot_point) {
 		return dr::rotate(angle, axes::y(), pivot_point);
 	}
 
-	inline Eigen::Affine3d rotate_z(double angle) {
+	inline Eigen::AngleAxisd rotate_z(double angle) {
 		return dr::rotate(angle, axes::z());
 	}
 
-	inline Eigen::Affine3d rotate_z(double angle, Eigen::Vector3d const & pivot_point) {
+	inline Eigen::Isometry3d rotate_z(double angle, Eigen::Vector3d const & pivot_point) {
 		return dr::rotate(angle, axes::z(), pivot_point);
 	}
 }
