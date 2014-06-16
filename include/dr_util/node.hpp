a72aae8d6660d2f4b39d9fed6dfc57b51633bfcc
bcalli <b.calli@tudelft.nl>
2014-06-16 10:17:07 +0200

    (split-dr-util)Node.hpp: Naming convension is competible with ROS and functions are added for reading lists from server.
    dr_camera: Changed accordingly

--- include/dr_util/node.hpp
+++ include/dr_util/node.hpp
@@ -7,7 +7,7 @@ namespace dr_util {
 
 /// Get a parameter from the ROS parameter server.
 template<typename T>
-T get_param(ros::NodeHandle const & node, std::string const & name) {
+T getParam(ros::NodeHandle const & node, std::string const & name) {
 	T value;
 	if (!node.getParam(name, value)) {
 		throw std::runtime_error(std::string("Failed to get parameter `") + name + "' (" + node.resolveName(name) + ").");
@@ -17,41 +17,101 @@ T get_param(ros::NodeHandle const & node, std::string const & name) {
 
 /// Get a parameter from the ROS parameter server.
 template<typename T>
-T get_param(ros::NodeHandle const & node, std::string const & name, T const & fallback) {
+T getParam(ros::NodeHandle const & node, std::string const & name, T const & fallback) {
 	T value;
-	return node.getParam(name, value) ? value : fallback;
+
+	if (!node.getParam(name, value)) {
+		printf("Failed to get parameter `%s' (%s). Using the default value.\n",name.c_str(), node.resolveName(name).c_str());
+		value = fallback;
+	}
+
+	return value;
 }
 
-class node {
+/// Get a list of parameters for the ROS parameter server.
+template<typename T>
+std::vector<T> getParamList(ros::NodeHandle const & node, std::string const & name){
+	XmlRpc::XmlRpcValue list_xml;
+	std::vector<T> values;
+	if (!node.getParam("objects_to_spawn",list_xml)) {
+		throw std::runtime_error(std::string("Failed to get parameter `") + name + "' (" + node.resolveName(name) + ").");
+	}
+	else {
+		if(list_xml.getType() != XmlRpc::XmlRpcValue::TypeArray)
+			throw std::runtime_error(std::string("Failed to get parameter `") + name + "' (" + node.resolveName(name) + "). Invalid type.");
+		for (int32_t i = 0; i < list_xml.size(); ++i) {
+			values.push_back(static_cast<T>(list_xml[i]));
+		}
+	}
+	return values;
+}
+
+template<typename T>
+std::vector<T> getParamList(ros::NodeHandle const & node, std::string const & name, std::vector<T> const & fallback){
+	XmlRpc::XmlRpcValue list_xml;
+	std::vector<T> values;
+	if (!node.getParam("objects_to_spawn",list_xml)) {
+		printf("Failed to get parameter `%s' (%s). Using the default value.\n",name.c_str(), node.resolveName(name).c_str());
+		values = fallback;
+	}
+	else {
+		if(list_xml.getType() != XmlRpc::XmlRpcValue::TypeArray)
+			throw std::runtime_error(std::string("Failed to get parameter `") + name + "' (" + node.resolveName(name) + "). Invalid type.");
+		for (int32_t i = 0; i < list_xml.size(); ++i) {
+			values.push_back(static_cast<T>(list_xml[i]));
+		}
+	}
+	return values;
+}
+
+class Node {
 protected:
 	/// The ROS node handle.
-	ros::NodeHandle node_handle;
+	ros::NodeHandle node_handle_;
 
 	/// Construct the node.
-	node() : node_handle("~") {};
+	Node() : node_handle_("~") {};
 
-	/// Get a paramter from the ROS parameter server.
+	/// Get a parameter from the ROS parameter server.
 	/**
 	 * If the parameter doesn't exist on the parameter server, an exception is thrown.
 	 */
 	template<typename T>
-	T get_param(std::string const & name) {
-		return dr_util::get_param<T>(node_handle, name);
+	T getParam(std::string const & name) {
+		return dr_util::getParam<T>(node_handle_, name);
 	}
 
-	/// Get a paramter from the ROS parameter server.
+	/// Get a parameter from the ROS parameter server.
 	/**
 	 * If the parameter doesn't exist on the parameter server, the fallback is returned.
 	 */
 	template<typename T>
-	T get_param(std::string const & name, T const & fallback) {
-		return dr_util::get_param<T>(node_handle, name, fallback);
+	T getParam(std::string const & name, T const & fallback) {
+		return dr_util::getParam<T>(node_handle_, name, fallback);
+	}
+
+	/// Get a list of parameters from the ROS parameter server.
+	/**
+	 * If the parameters do not exist on the parameter server, an exception is thrown.
+	 */
+	template<typename T>
+	std::vector<T> getParamList(std::string const & name) {
+		return dr_util::getParamList<T>(node_handle_, name);
+	}
+
+	/// Get a list of parameters from the ROS parameter server.
+	/**
+	 * If the parameters do not exist on the parameter server, the fallback is returned.
+	 */
+	template<typename T>
+	std::vector<T> getParamList(std::string const & name, T const & fallback) {
+		return dr_util::getParamList<T>(node_handle_, name, fallback);
 	}
 
 public:
 	/// Check if ROS thinks we should keep running.
 	bool ok() {
-		return node_handle.ok();
+		return node_handle_.ok();
 	}
 };
 
