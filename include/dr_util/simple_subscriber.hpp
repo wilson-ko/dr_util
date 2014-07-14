cabc603672da33b920aeadc2966ecae740e8be71
bcalli <b.calli@tudelft.nl>
2014-07-14 17:57:54 +0200

    (split-dr-util)simple_subscriber: add an initialization function. Add checks for receiving first message or initializing the object.

--- include/dr_util/simple_subscriber.hpp
+++ include/dr_util/simple_subscriber.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <string>
-
 #include <ros/ros.h>
 
 namespace dr {
@@ -15,8 +14,12 @@ private:
 
 	/// The internal subscriber used.
 	ros::Subscriber subscriber_;
+	std::string topic_name_ = "";
+	bool message_received_ = false;
 
 public:
+	SimpleSubscriber(){
+	}
 	/// Construct the subscriber.
 	SimpleSubscriber(
 		ros::NodeHandle & node,                  ///< The node handle to use for name resolution.
@@ -24,16 +27,33 @@ public:
 		uint32_t queue_size = 10,                ///< The queue size for the wrapper subscriber.
 		ros::TransportHints transport_hints = {} ///< The transport hints for the wrapper subscriber.
 	) {
+		initialize(node, topic,queue_size,transport_hints);
+	}
+
+	void initialize(ros::NodeHandle & node,      ///< The node handle to use for name resolution.
+		std::string const & topic,               ///< The name of the topic to subscribe to.
+		uint32_t queue_size = 10,                ///< The queue size for the wrapper subscriber.
+		ros::TransportHints transport_hints = {} ///< The transport hints for the wrapper subscriber.
+		){
 		subscriber_ = node.subscribe(topic, queue_size, &SimpleSubscriber::onMessage, this, transport_hints);
-	};
+		topic_name_ = topic;
+	}
 
 	/// Get the last message received.
-	Message const & message() const { return message_; };
+	Message const & message() const { 
+		if(!message_received_)
+			ROS_ERROR("No messages has been received to the topic %s, but the message is requested.",topic_name_.c_str());
+		if(topic_name_ == "")
+			ROS_ERROR("Subscriber not initialized, but the message is requested.");
+		return message_; 
+	}
 
 private:
 	/// Handle received messages.
 	void onMessage(Message const & message) {
 		message_ = message;
+		if(!message_received_)
+			message_received_ = true;
 	}
 };
 
