62b17b8ac3acc00e7ad8a6c919c899ab090de4e3
Maarten de Vries <maarten@de-vri.es>
2014-11-20 13:59:27 +0100

    (split-dr-util)util: Add comments to bitwise.hpp

--- include/dr_util/bitwise.hpp
+++ include/dr_util/bitwise.hpp
@@ -2,9 +2,27 @@
 #include <stdint.h>
 
 namespace dr {
-	constexpr uint32_t bitMask(unsigned int bit) { return 1 << bit; };
+	/// Get the bitmask for a single bit.
+	constexpr uint32_t bitMask(
+		unsigned int bit ///< The index of the bit to generate the mask for, where 0 is the least significant bit.
+	) {
+		return 1 << bit;
+	}
 
-	constexpr bool testBit(uint32_t values, unsigned int bit) { return values & bitMask(bit); };
+	/// Test the value of a specific bit in an integer.
+	constexpr bool testBit(
+		uint32_t value,  ///< The value to test.
+		unsigned int bit ///< The index of the bit to test, where 0 is the least significant bit.
+	) {
+		return value & bitMask(bit);
+	}
 
-	constexpr bool testRising(uint32_t values, uint32_t old, unsigned int bit) { return (values & bitMask(bit)) && !(old & bitMask(bit)); };
+	/// Test for a newly high value of a specific bit.
+	constexpr bool testRising(
+		uint32_t value,  ///< The new value to test.
+		uint32_t old,    ///< The old value.
+		unsigned int bit ///< The index of the bit to test, where 0 is the least significant bit.
+	) {
+		return (value & bitMask(bit)) && !(old & bitMask(bit));
+	}
 };
