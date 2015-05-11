aeff6626d92d0522092cb8339ce8c6858c90d85d
Ronald <r.m.ensing@delftrobotics.com>
2015-05-11 11:28:11 +0200

    (split-dr-util)dr_vision_tools: Move file system code to dr_util

--- include/dr_util/file_system.hpp
+++ include/dr_util/file_system.hpp
@@ -13,6 +13,25 @@ namespace dr {
 	std::vector<std::string> getFilesInDirectoryRecursive(std::string const & directory);
 	std::vector<std::string> getFilesInDirectoryRecursive(std::string const & directory, std::string const & suffix);
 
+	/// Gets a list of all file names from a given directory.
+	void getFilesInDirectory(
+		const std::string & directory,              ///< Input directory path
+		std::vector<std::string> & file_names       ///< Vector of filenames contained in the directory
+	);
+
+	/// Gets a list of all file names from a given directory.
+	void getFilesInDirectory(
+		const std::string & directory,
+		std::vector<std::string> & file_names,
+		std::string suffix
+	);
+
+	/// Gets a list of all file names from a given directory.
+	std::vector<std::string> getFilesInDirectory(
+		const std::string & directory,
+		std::string suffix
+	);
+
 	/// Returns true if a given string has a given suffix.
 	bool hasSuffix(
 		const std::string & string,                 ///< Input string
