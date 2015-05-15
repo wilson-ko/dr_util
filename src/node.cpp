8553b4285fd6cf5889fbae20124e3ae4d3ccf0c2
Maarten de Vries <maarten@de-vri.es>
2015-05-15 12:24:26 +0200

    (split-dr-util)log: Tweak alignment.
    refs #781

--- src/node.cpp
+++ src/node.cpp
@@ -18,9 +18,9 @@ std::string namespaceToName(std::string const & name) {
 
 Node::Node() : node_handle_("~") {
 	std::string node_name = namespaceToName(node_handle_.getNamespace());
-	run_prefix_  = searchParam<std::string>("run_prefix", getHomeDirectory() + "/.ros/log");
-	node_prefix_ = run_prefix_ + "/" + node_name;
-	std::string log_file = node_prefix_ + "/" + node_name + ".log";
+	run_prefix_           = searchParam<std::string>("run_prefix", getHomeDirectory() + "/.ros/log");
+	node_prefix_          = run_prefix_  + "/" + node_name;
+	std::string log_file  = node_prefix_ + "/" + node_name + ".log";
 
 	setupLogging(log_file, node_name);
 };
