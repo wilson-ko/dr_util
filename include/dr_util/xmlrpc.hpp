27acaed9df34caf49fee8be07f12d60937048c05
Maarten de Vries <maarten@de-vri.es>
2015-06-01 15:57:15 +0200

    (split-dr-util)util: Add support for loading maps from XmlRpcValues.
    refs #908 @30m

--- include/dr_util/xmlrpc.hpp
+++ include/dr_util/xmlrpc.hpp
@@ -19,6 +19,20 @@ std::string xmlRpcTypeName(XmlRpc::XmlRpcValue::Type type);
 template<typename T>
 struct ConvertXmlRpc;
 
+/// Load a value from a XmlRpcValue.
+/**
+ * Needs static T ConvertXmlRpc<T>::convert(XmlRpc::XmlRpcValue const &) to be defined.
+ *
+ * \throws Anything ConvertXmlRpc<T>::convert() throws.
+ * \return The loaded value.
+ */
+template<typename T>
+T fromXmlRpc(
+	XmlRpc::XmlRpcValue const & value ///< The XmlRpcValue to load from.
+) {
+	return ConvertXmlRpc<T>::convert(value);
+}
+
 template<>
 struct ConvertXmlRpc<bool> {
 	static bool convert(XmlRpc::XmlRpcValue const & value);
@@ -42,11 +56,29 @@ struct ConvertXmlRpc<std::string> {
 template<typename T>
 struct ConvertXmlRpc<std::vector<T>> {
 	static std::vector<T> convert(XmlRpc::XmlRpcValue const & value) {
-		if (value.getType() != XmlRpc::XmlRpcValue::TypeArray) throw std::runtime_error("Cannot convert XmlRpc type " + xmlRpcTypeName(value.getType()) + " to vector.");
+		if (value.getType() != XmlRpc::XmlRpcValue::TypeArray) throw std::runtime_error("Cannot convert XmlRpc type " + xmlRpcTypeName(value.getType()) + " to a vector.");
 
 		std::vector<T> result;
 		for (int i = 0; i < value.size(); ++i) {
 			result.push_back(ConvertXmlRpc<T>::convert(value));
+		}
+
+		return result;
+	}
+};
+
+template<typename T>
+struct ConvertXmlRpc<std::map<std::string, T>> {
+	static std::map<std::string, T> convert(XmlRpc::XmlRpcValue const & value) {
+		if (value.getType() != XmlRpc::XmlRpcValue::TypeStruct) throw std::runtime_error("Cannot convert XmlRpc type " + xmlRpcTypeName(value.getType()) + " to a map.");
+
+		// Very ugly hack to work around missing const access to map.
+		// TODO: Remove once ROS updates to newer XmpRpc++ with const accessors.
+		XmlRpc::XmlRpcValue & ugly_hack = const_cast<XmlRpc::XmlRpcValue &>(value);
+
+		std::map<std::string, T> result;
+		for (XmlRpc::XmlRpcValue::ValueStruct::const_iterator i = ugly_hack.begin(); i != ugly_hack.end(); ++i) {
+			result.insert({i->first, fromXmlRpc<T>(i->second)});
 		}
 
 		return result;
