e30f8f06cacb5e684c4c1cca1edbf87f7186282d
Ronald Ensing <ronaldensing@gmail.com>
2016-10-21 10:53:53 +0200

    Add overloaded functions to get time string.

--- include/dr_util/timestamp.hpp
+++ include/dr_util/timestamp.hpp
@@ -1,7 +1,20 @@
 #pragma once
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <string>
 
 namespace dr {
+
+	// Get a string representing a posix ptime in the specified format
+	std::string formatTime(
+		boost::posix_time::ptime timestamp,
+		std::string const & format
+	);
+
+	/// Get a string representing the time according to a specified format.
+	std::string getTimeString(
+		std::string const & format ///< A boost::date_time compatible format string.
+	);
+
 	/// Creates a string with the current time.
 	void getTimeString(std::string & output);
 
