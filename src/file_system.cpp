6c7f4754c7b528a29047d329c46b5be628ef7892
Wilson Ko <w.k.h.ko91@gmail.com>
2019-08-07 01:56:23 +0900

    Fix clang-tidy.

--- src/file_system.cpp
+++ src/file_system.cpp
@@ -10,6 +10,9 @@
 
 namespace dr {
 
+constexpr std::size_t BUFFER_SIZE = 256;
+constexpr std::size_t BUFFER_THRESHOLD = 1024;
+
 bool createDirectory(std::string const & path){
 	boost::system::error_code error;
 	return boost::filesystem::create_directories(path, error);
@@ -24,8 +27,9 @@ std::vector<std::string> getFilesInDirectoryRecursive(std::string const & direct
 	boost::filesystem::path start_at(directory);
 	boost::filesystem::recursive_directory_iterator iterator(start_at);
 	while(iterator != boost::filesystem::recursive_directory_iterator()){ // Iterate over all directories recursively.
-		if ( boost::filesystem::is_regular_file(iterator->path()) ) // Only return file names, not names of the directories.
+		if ( boost::filesystem::is_regular_file(iterator->path()) ){ // Only return file names, not names of the directories.
 			files.push_back(iterator->path().string());
+		}
 		++iterator;
 	}
 	return files;
@@ -34,43 +38,48 @@ std::vector<std::string> getFilesInDirectoryRecursive(std::string const & direct
 std::vector<std::string> getFilesInDirectoryRecursive(std::string const & directory, std::string const & suffix){
 	std::vector<std::string> all_files = getFilesInDirectoryRecursive(directory);
 	std::vector<std::string> subset_files;
-	for (size_t i=0; i<all_files.size(); i++){
-		if (hasSuffix(all_files.at(i), suffix)){
-			subset_files.push_back(all_files.at(i));
+	for (auto const & file : all_files) {
+		if (hasSuffix(file, suffix)){
+			subset_files.push_back(file);
 		}
 	}
 	return subset_files;
 }
 
 bool hasSuffix(const std::string & string, const std::string & suffix) {
-	return string.size() >= suffix.size() && !string.compare(string.size() - suffix.size(), suffix.size(), suffix);
+	return string.size() >= suffix.size() && !static_cast<bool>(string.compare(string.size() - suffix.size(), suffix.size(), suffix));
 }
 
 std::string getHomeDirectory(std::string const & fallback) {
 	char const * homedir = std::getenv("HOME");
-	if (homedir) return homedir;
+	bool homedir_empty = static_cast<bool>(homedir);
+	if (homedir_empty) { return homedir; }
 
-	std::vector<std::uint8_t> buffer(256);
+	std::vector<std::uint8_t> buffer(BUFFER_SIZE);
 	uid_t uid = getuid();
-	passwd info;
+	passwd info{};
 	passwd * result;
 
 	while (true) {
 		errno = 0;
 		int error = getpwuid_r(uid, &info, reinterpret_cast<char *>(buffer.data()), buffer.size(), &result);
 
-		if (!error) {
+		if (!static_cast<bool>(error)) {
 			return result != nullptr ? info.pw_dir : fallback;
-		} else if (errno != ERANGE) {
-			break;
-		} else if (buffer.size() >= 1024) {
-			break;
-		} else {
-			buffer.resize(buffer.size() * 2);
 		}
+
+		if (errno != ERANGE) {
+			break;
+		}
+
+		if (buffer.size() >= BUFFER_THRESHOLD) {
+			break;
+		}
+
+		buffer.resize(buffer.size() * 2);
 	}
 
 	return fallback;
 }
 
-}
+} //namespace dr
--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -10,7 +10,7 @@ double Polynomial::Term::y(double x) const noexcept {
 
 double Polynomial::y(double x) const noexcept {
 	double result = 0;
-	for (Term const & term : terms) result += term.y(x);
+	for (Term const & term : terms) { result += term.y(x); }
 	return result;
 }
 
@@ -18,8 +18,8 @@ Polynomial Polynomial::derivative() const {
 	Polynomial derived;
 
 	for (Term const & term : terms) {
-		if (term.coefficient() == 0 || term.exponent() == 0) continue;
-		derived.terms.push_back(Term(term.coefficient() * term.exponent(), term.exponent() - 1));
+		if (term.coefficient() == 0 || term.exponent() == 0) { continue; }
+		derived.terms.emplace_back(Term(term.coefficient() * term.exponent(), term.exponent() - 1));
 	}
 
 	return derived;
@@ -28,10 +28,10 @@ Polynomial Polynomial::derivative() const {
 std::ostream & operator<< (std::ostream & stream, Polynomial const & polynomial) {
 	for (Polynomial::Term const & term : polynomial.terms) {
 		stream << term.coefficient() << "x" << "^" << term.exponent();
-		if (&term != &polynomial.terms.back()) stream << " + ";
+		if (&term != &polynomial.terms.back()) { stream << " + "; }
 	}
 
 	return stream;
 }
 
-}
+} //namespace dr
